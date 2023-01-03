c224fea4118d2a70a7e446393c72fda3e597c875
starydv7 <pawanstar72926@gmail.com>
2023-01-03 23:29:28 +0530

    React Table

---
+++ src/components/Table.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { useTable } from "react-table";
+
+export default class Table extends Component {
+  render() {
+    return (
+      <div>Table</div>
+    )
+  }
+}
