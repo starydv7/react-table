964fb6a5698ccd3c1907df85977715ffad4d3043
starydv7 <pawanstar72926@gmail.com>
2023-01-04 00:30:22 +0530

    react table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,9 +2,48 @@ import React, { Component } from 'react';
 import { useTable } from "react-table";
 
 export default class Table extends Component {
-  render() {
-    return (
-      <div>Table</div>
+    columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
     )
+    
+  render() {
+    
+    return <Table columns={this.columns} data={this.data} />;
   }
 }
