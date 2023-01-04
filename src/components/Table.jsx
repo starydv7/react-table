6eec7e61ba4b4f01b221560275ac9d40e8903062
starydv7 <pawanstar72926@gmail.com>
2023-01-04 10:48:20 +0530

    table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -12,11 +12,51 @@ export default class MyList extends React.Component {
               console.log(posts);
     },[]);
   }
+ columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
   render() {
     return (
       <div>
         <h2>Fetch table data</h2>
+       
       </div>
-    )
+    );
   }
 }
