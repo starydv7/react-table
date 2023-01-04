7c11b610bf293aac1aba02119d831f27d5604c36
starydv7 <pawanstar72926@gmail.com>
2023-01-04 11:27:31 +0530

    table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,24 +3,36 @@ import axios from "axios";
 import { useTable } from "react-table";
 export default class MyList extends React.Component {
   state = {
-    blogs: [],
+    books: [],
   };
-  componentDidMount() {
-      axios.get(`https://dummyjson.com/users`)
-          .then((response) => {
-      const posts = response.data.users;
-       this.setState({posts });
-              console.log(posts);
-    },[]);
-  }
-  render() {
-    return (
-      <div>
-        <h2>Fetch table data</h2>
-        {this.state.blogs.map((item, i) => {
-          return <div key={item.id}>{item.firstName}</div>;
-       })}
-      </div>
-    );
-  }
+   fetchBooks = () => {
+        fetch('https://dummyjson.com/users')
+        .then((response) => response.json())
+        .then(booksList => {
+            this.setState({ books: booksList });
+        });
+    }
+  // componentDidMount() {
+  //   axios.get(`https://dummyjson.com/users`).then((response) => {
+  //     const posts = response.data.users;
+  //     this.setState({ posts });
+  //     //console.log(posts);
+  //   }, []);
+  // }
+   render() {
+        return (
+        <>
+            <button onClick={this.fetchBooks}>Load Books</button>
+            {this.books.length > 0 && (
+                <ul>
+                    {this.state.books.map((book) => (
+                        <li key={book.id}>{book.firstName}</li>
+                    ))}
+                </ul>
+            )}
+            
+        </>
+        )
+    }
 }
+
